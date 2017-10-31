dba4c4684d64343fafdac933f4fc2b1aadf68eda
Shang-Hung Tsai <shtsai7@gmail.com>
2017-10-30 20:42:51 -0700

    add checkIntersection() and findIntersection() unfinished

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -25,6 +25,9 @@ Mat mySobel(Mat mat);
 Mat myThresholding(Mat mat, int threshold);
 unordered_map<int, vector<int> > myHoughTransform(Mat mat);
 void addLine(Mat mat, int p, int theta); 
+void detectParallelogram(unordered_map<int, vector<int> > lines, Mat edges);
+bool checkIntersection(int theta1, int a1p1, int a1p2, int theta2, int a2p1, int a2p2, Mat mat); 
+bool findIntersection (int theta1, int p1, int theta2, int p2, Mat mat, vector<pair<int,int>> intersections);
 
 
 /*  This function converst a RGB Mat object into a grayscale Mat.  */
@@ -141,6 +144,9 @@ Mat myThresholding(Mat mat, int threshold)
  * Here the angle is defined as the angle between perpendicular line 
  * and the positve x axis, counter-clockwise.
  * This is because we are using (i,j) coordinate system.
+ *
+ * This function returns an unorder map, where key is theta and value is a
+ * list of p values. 
  */
 unordered_map<int, vector<int> > myHoughTransform(Mat mat) 
 {
@@ -213,7 +219,8 @@ unordered_map<int, vector<int> > myHoughTransform(Mat mat)
  * Given p and theta value, this function adds a line that satisfies the condition
  * to the input mat.
  */
-void addLine(Mat mat, int p, int theta) {
+void addLine(Mat mat, int p, int theta) 
+{
     double COS = cos(theta * PI / 180);
     double TAN = tan(theta * PI / 180);
 
@@ -223,6 +230,87 @@ void addLine(Mat mat, int p, int theta) {
 	    mat.at<uchar>(i, j) = 255;
 	}
     }
+}
+
+void detectParallelogram(unordered_map<int, vector<int> > lines, Mat edges) 
+{   
+    // put all angles in an vector
+    vector<int> angles;
+    for (unordered_map<int, vector<int> >::iterator it = lines.begin(); it != lines.end(); it++)
+    {
+	cout << it->first << " ";
+	for (int i = 0; i < it->second.size(); i++) {
+	    cout << it->second.at(i) << " ";
+	}
+	cout << endl;
+	angles.push_back(it->first);
+    }
+
+    for (int a1 = 0; a1 < angles.size(); a1++) {  // first angle
+	for (int a2 = a1 + 1; a2 < angles.size(); a2++) { // second angle 
+	    for (int a1p1 = 0; a1p1 < lines[angles.at(a1)].size(); a1p1++) {
+		for (int a1p2 = a1p1 + 1; a1p2 < lines[angles.at(a1)].size(); a1p2++) {
+		    for (int a2p1 = 0; a2p1 < lines[angles.at(a2)].size(); a2p1++) {
+			for (int a2p2 = a2p1+1; a2p2 < lines[angles.at(a2)].size(); a2p2++) {
+			    int theta1 = angles.at(a1);
+			    int theta2 = angles.at(a2);
+			    if (checkIntersection(theta1, a1p1, a1p2, theta2, a2p1, a2p2, edges)) {
+				//cout << "Find parallelogram" << endl;
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+bool checkIntersection(int theta1, int a1p1, int a1p2, int theta2, int a2p1, int a2p2, Mat mat) 
+{
+    vector<pair<int,int> > intersections;
+
+    if (!findIntersection(theta1, a1p1, theta2, a2p1, mat, intersections)) {
+	return false;
+    }
+    if (!findIntersection(theta1, a1p1, theta2, a2p2, mat, intersections)) {
+	return false;
+    }
+    if (!findIntersection(theta1, a1p2, theta2, a2p1, mat, intersections)) {
+	return false;
+    }
+    if (!findIntersection(theta1, a1p2, theta2, a2p2, mat, intersections)) {
+	return false;
+    }
+
+    if (intersections.size() > 0) {
+    for (int i = 0; i < intersections.size(); i++) {
+	pair<int, int> p = intersections.at(i);
+	cout << p.first << " + " << p.second << endl;
+    }
+    cout << endl;
+    }
+    return true;
+}
+
+bool findIntersection (int theta1, int p1, int theta2, int p2, Mat mat, vector<pair<int,int>> intersections)
+{
+    double i, j;
+    int inti, intj;
+
+    i = (cos(theta1*PI/180)*p2 - cos(theta2*PI/180)*p1) 
+	/ (sin(theta2*PI/180)*cos(theta1*PI/180)-sin(theta1*PI/180)*cos(theta2*PI/180));
+    j = (p1 - i * sin(theta1*PI/180)) / cos(theta1*PI/180);
+
+    inti = (int) i;
+    intj = (int) j;
+
+    if (mat.at<uchar>(i, j) != 0) {
+	return false;    // no edge at intersection
+    } 
+    
+    pair<int, int> point (inti, intj);
+    intersections.push_back(point);
+    return true;   
 }
 
 int main(int argc, char** argv) 
@@ -295,6 +383,10 @@ int main(int argc, char** argv)
 
     // Hough Transform
     unordered_map<int, vector<int> > lines = myHoughTransform(edges); 
+
+    // Detect Parallelogram
+    detectParallelogram(lines, edges);
+    
 
     return 0;
 }
