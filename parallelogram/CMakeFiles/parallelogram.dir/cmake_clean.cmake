f504411b0782aac76dbc6dcb69aaa3b909c54bae
Shang-Hung Tsai <shtsai7@gmail.com>
2017-12-24 10:26:26 -0500

    clean up

---
+++ parallelogram/CMakeFiles/parallelogram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallelogram.dir/parallelogram.cpp.o"
+  "parallelogram.pdb"
+  "parallelogram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parallelogram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
